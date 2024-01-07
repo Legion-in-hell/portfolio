2ba47d274785d1535183b4a591ee96288f20e1b6
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-07 13:28:10 +0100

    Add Github component to display coding activity

---
+++ src/components/About/Github.js
@@ -0,0 +1,22 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+
+function Github() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+        Les jours où j'ai <strong className="purple">coder</strong>
+      </h1>
+      <GitHubCalendar
+        username="Legion-in-Hell"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+      />
+    </Row>
+  );
+}
+
+export default Github;
