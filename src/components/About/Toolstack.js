fb13c34ac1990faa7a49cd639a49a79223c02a62
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-07 13:28:04 +0100

    Add Toolstack component

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiWindows
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
