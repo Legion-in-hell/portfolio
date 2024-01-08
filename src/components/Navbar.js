498fc7a273ff424fdfe189be2c021702cafb8c96
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-08 03:32:36 +0100

    Fix button styling in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,8 +97,8 @@ function NavBar() {
                 target="_blank"
                 className="fork-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
+                <CgGitFork style={{ fontSize: "1.2em"}} />
+                <AiFillStar style={{ fontSize: "1.1em"}} />
               </Button>
             </Nav.Item>
           </Nav>
